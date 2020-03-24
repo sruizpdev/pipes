c59b2fefa6a8827cefc7c499d4562c2b95517eaa
Sergio Ruiz <sergiotto@outlook.com>
2020-03-24 12:16:09 +0100

    mas pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,15 @@ export class AppComponent {
   nombre = 'Capitán América';
   arreglo = [1,2,3,4,5,6,7,8];
   PI: number = Math.PI;
+  porcentaje:number = 0.234;
+  salario:number= 1234.5;
+  heroe={
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
 }
